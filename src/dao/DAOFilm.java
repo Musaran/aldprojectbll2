074bdbd300183b98e501b76f7a13737622240578
matthieu.lauer <matthieu.lauer@110833f5-2527-69ae-659e-ae615b011d9b>
2010-03-31 11:55:50 +0000

    Création des DAOFilm, DAOPersonne et DAOProfessionnel

---
+++ src/dao/DAOFilm.java
@@ -0,0 +1,23 @@
+package dao;
+
+import java.util.Set;
+
+import metier.Film;
+/**
+ * 
+ * @author Matthieu
+ * DAO de film.   
+ *
+ */
+public interface DAOFilm {
+
+	Film			get(int code)					throws Exception;
+	/// liste des Film ayant pour titre le paramètre donné
+	Set<Film>		load(String titre)				throws Exception;
+	/// liste de toutes les Films 
+	Set<Film>		loadAll()						throws Exception;
+	void				save(Film film)				throws Exception;
+	void				saveOrUpdate(Film film)		throws Exception;
+	void				remove(Film film)			throws Exception;
+	void 				clear()						throws Exception;
+}
