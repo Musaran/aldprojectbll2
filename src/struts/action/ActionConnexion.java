9f9c37124601b9f9bd20729f744817a30c1594a0
matthieu.lauer <matthieu.lauer@110833f5-2527-69ae-659e-ae615b011d9b>
2010-05-10 09:03:42 +0000

    Avancement dans la connexion d'un pro. mais erreur dans l'action. ça doit être dans struts-config.xml le problème.

---
+++ src/struts/action/ActionConnexion.java
@@ -0,0 +1,33 @@
+package struts.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import metier.Professionnel;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import struts.actionForm.ActionFormConnexion;
+
+import dao.DAOProfessionnel;
+
+public class ActionConnexion extends Action{
+	private DAOProfessionnel daoPro=null;
+	
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response){
+		ActionFormConnexion connexion=(ActionFormConnexion)form;
+		try {
+			Professionnel pro=daoPro.get(connexion.getLogin());
+			request.setAttribute("SESSION", pro);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return mapping.findForward("ConnexionEffectue");
+		
+	}
+}
