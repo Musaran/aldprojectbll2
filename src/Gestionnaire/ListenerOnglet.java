562b87ab4197d17a81c7791761f3819495c3c127
jeanphilippe.blaise <jeanphilippe.blaise@110833f5-2527-69ae-659e-ae615b011d9b>
2010-05-11 20:24:53 +0000

    Correctif Navigateur Web pour eviter des exceptions

--- src/Gestionnaire/ListenerOnglet.java
+++ src/Gestionnaire/ListenerOnglet.java
@@ -1,5 +1,7 @@
 package Gestionnaire;
 
+import java.io.IOException;
+
 import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -16,10 +18,19 @@ public class ListenerOnglet implements ChangeListener{
 		JTabbedPane source= (JTabbedPane)arg0.getSource();
 		
 		switch(source.getSelectedIndex()) {
-		case 1: GestionnaireRoleActeur gestact = new GestionnaireRoleActeur(); gestact.refresh(); break;
-		case 2: GestionnaireRoleProducteur gestprod = new GestionnaireRoleProducteur(); gestprod.refresh();break;
-		case 3: GestionnaireRoleRealisateur gestreal = new GestionnaireRoleRealisateur(); gestreal.refresh();break;
-		case 4: GestionnaireFilm gestfilm = new GestionnaireFilm(); gestfilm.refresh();break;
+			case 1: GestionnaireRoleActeur gestact = new GestionnaireRoleActeur(); gestact.refresh(); break;
+			case 2: GestionnaireRoleProducteur gestprod = new GestionnaireRoleProducteur(); gestprod.refresh();break;
+			case 3: GestionnaireRoleRealisateur gestreal = new GestionnaireRoleRealisateur(); gestreal.refresh();break;
+			case 4: GestionnaireFilm gestfilm = new GestionnaireFilm(); gestfilm.refresh();break;
+			case 8: {
+				try {
+					AffichageGestionnaire.getInstance().getjEditorPaneNavigateur().setPage("http://localhost:8080/sitewebald/vues/index.jsp");
+					AffichageGestionnaire.getInstance().getTxtURL().setText("http://localhost:8080/sitewebald/vues/index.jsp");
+				} 
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
