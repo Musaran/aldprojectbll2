aa2c684bdebd67c83e31d253ca7181fd9a7adcb6
matthieu.lauer <matthieu.lauer@110833f5-2527-69ae-659e-ae615b011d9b>
2010-05-23 16:55:31 +0000

    liste des recompenses pr films et personnes mais je ne peux que afficher l'année car les autres champs sont des recompenses ou des types de recompenses, je peux pas les afficher.
    Il me faudrait donc ces fontions avec en données membres des string pour les noms des recompenses et le nom du type de recompense et l'année. :) Je sais pas si c'est possible

--- trunk/src/struts/action/ActionApercuFilm.java
+++ trunk/src/struts/action/ActionApercuFilm.java
@@ -1,12 +1,14 @@
 package struts.action;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import metier.Film;
 import metier.Personne;
+import metier.RecompenseFilm;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
@@ -18,17 +20,20 @@ import dao.DAOFilm;
 public class ActionApercuFilm extends Action{
 	private DAOFilm daoFilm=null;
 	
+	@SuppressWarnings("unchecked")
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
 			HttpServletResponse response){
 		Film film=null;
 		ArrayList<Personne> acteurs=null, realisateurs=null, producteurs=null;
+		Set<RecompenseFilm> recompenses;
 		try {
 			film=daoFilm.get(Integer.valueOf(request.getParameter("code")));
 			acteurs=film.listeDesActeurs();
 			realisateurs=film.listeDesRealisateurs();
 			producteurs=film.listeDesProducteurs();
-
+			recompenses=film.getListeRecompense();
 			request.getSession().setAttribute("FILM", film);
+			request.getSession().setAttribute("RECOMPENSES", recompenses);
 			request.getSession().setAttribute("ACTEURS", acteurs);
 			request.getSession().setAttribute("PRODUCTEURS",producteurs );
 			request.getSession().setAttribute("REALISATEURS",realisateurs );
